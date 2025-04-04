8916a5fb1597df42bdc2a7dfabdb8a992519cc3e
ultimatemeong <123579639+ultimatemeong@users.noreply.github.com>
2025-04-05 00:40:02 +0800

    Update Project.java

--- src/Projects/Project.java
+++ src/Projects/Project.java
@@ -1,11 +1,9 @@
 package Projects;
 
+import Users.*;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
-
-import Users.*;
-import Misc.*;
 
 public class Project {
     private String name;
@@ -13,42 +11,43 @@ public class Project {
     private String[] flatTypes; // e.g. 2-room, 3-room
     private int[] sellingPrices; // e.g. 2-room: 300k, 3-room: 500k
     private int[] availableUnits; // e.g. 2-room: 100, 3-room: 50
-    private LocalDate applicationStartDate; // e.g. 2023-01-01  (Does not need Date class, as we done need it to be too precise)
-    private LocalDate applicationEndDate; // e.g. 2023-01-31  
-    private boolean isVisible;
-
-
-    private List<String> ApplicantList = new ArrayList<>();
-
-    private int officerSlots = 10;
-    private List<String> OfficerList = new ArrayList<>();
+    private LocalDate applicationOpenDate; // e.g. 2023-01-01  (Does not need Date class, as we done need it to be too precise)
+    private LocalDate applicationCloseDate; // e.g. 2023-01-31  
+    private boolean visibility;
 
     private String manager;
 
-    public Project(String name, String neighbourhood, String[] flatTypes, int[] sellingPrices, int[] availableUnits, LocalDate applicationStartDate, LocalDate applicationEndDate) {
+    private int officerSlots = 10;
+    private List<String> officerList = new ArrayList<>();
+    
+    private List<String> applicationList = new ArrayList<>();
+
+    public Project(String name, String neighbourhood, String[] flatTypes, int[] sellingPrices, int[] availableUnits, LocalDate applicationOpenDate, LocalDate applicationCloseDate) {
         this.name = name;
         this.neighbourhood = neighbourhood;
         this.flatTypes = flatTypes;
         this.sellingPrices = sellingPrices;
         this.availableUnits = availableUnits;
-        this.applicationStartDate = applicationStartDate;
-        this.applicationEndDate = applicationEndDate;
-        this.isVisible = true; // Default to visible
+        this.applicationOpenDate = applicationOpenDate;
+        this.applicationCloseDate = applicationCloseDate;
+        this.visibility = true; // Default to visible
     }
 
+    public boolean isVisible() {return visibility;}
+
     public void addOfficer(Officer officer){
-        if (OfficerList.size() < officerSlots) {
-            OfficerList.add(officer.getName());
+        if (officerList.size() < officerSlots) {
+            officerList.add(officer.getName());
         } else {
             System.out.println("No more slots available for officers.");
         }
     }
 
     public List<String> getOfficerList() {
-        return OfficerList;
+        return officerList;
     }
 
     public List<String> getApplicantList() {
-        return ApplicantList;
+        return applicationList;
     }
 }
