85f261679d1863e96ca1dd3fcc31bdad0ed6d004
ultimatemeong <123579639+ultimatemeong@users.noreply.github.com>
2025-04-11 11:20:41 +0800

    edited Project class and added createProject for Manager

--- src/Projects/Project.java
+++ src/Projects/Project.java
@@ -3,25 +3,49 @@ package Projects;
 import Users.*;
 import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Project {
     private String name;
     private String neighbourhood;
-    private String[] flatTypes; // e.g. 2-room, 3-room
-    private int[] sellingPrices; // e.g. 2-room: 300k, 3-room: 500k
-    private int[] availableUnits; // e.g. 2-room: 100, 3-room: 50
+    private Map<String, List<Integer>> flatsInfo = new HashMap<>(); // key: flat type (e.g. 2-room, 3-room), value: [selling price, avail units]
+
     private LocalDate applicationOpenDate; // e.g. 2023-01-01  (Does not need Date class, as we done need it to be too precise)
     private LocalDate applicationCloseDate; // e.g. 2023-01-31  
     private boolean visibility;
 
-    private String manager;
+    private Manager manager;
 
     private int officerSlots = 10;
     private List<String> officerList = new ArrayList<>();
     
     private List<String> applicationList = new ArrayList<>();
 
+    public Project(String name, String neighbourhood, String unitType1, Integer numUnitsType1, Integer priceType1, String unitType2, Integer numUnitsType2, Integer priceType2,
+            LocalDate applicationOpenDate, LocalDate applicationCloseDate, boolean visibility, Manager manager,
+            int officerSlots, List<String> officerList) {
+        this.name = name;
+        this.neighbourhood = neighbourhood;
+
+        this.applicationOpenDate = applicationOpenDate;
+        this.applicationCloseDate = applicationCloseDate;
+        this.visibility = visibility;
+        this.manager = manager;
+        this.officerSlots = officerSlots;
+        this.officerList = officerList;
+
+        List<Integer> type1Info = Arrays.asList(numUnitsType1, priceType1);
+        List<Integer> type2Info = Arrays.asList(numUnitsType2, priceType2);
+
+        HashMap<String, List<Integer>> flatsInfo = new HashMap<String, List<Integer>>();
+        flatsInfo.put(unitType1, type1Info);
+        flatsInfo.put(unitType2, type2Info);
+
+        this.flatsInfo = flatsInfo;
+    }
 
     public String getName() {
         return name;
--- src/Users/Manager.java
+++ src/Users/Manager.java
@@ -1,5 +1,9 @@
 package Users;
 
+import Projects.*;
+import java.time.LocalDate;
+import java.util.List;
+
 public class Manager extends User{
 
     public Manager(String name, String nric, int age, String maritalStatus, String password) {
@@ -16,8 +20,9 @@ public class Manager extends User{
         // View all projects regardless of visibility
     }
 
-    public void createProject() {
-        // Logic for creating projects
+    public void createProject(String proj_name, String neighbourhood, String unitType1, Integer numUnitsType1, Integer priceType1, String unitType2, Integer numUnitsType2, Integer priceType2, LocalDate appOpenDate, LocalDate appCloseDate, boolean visibility, Integer officerSlots, List<String> officerList) {
+        Project project = new Project(proj_name, neighbourhood, unitType1, numUnitsType1, priceType1, unitType2, numUnitsType2, priceType2, appOpenDate, appCloseDate, visibility, this, officerSlots, officerList);
+
     }
 
     public void editProject() {
