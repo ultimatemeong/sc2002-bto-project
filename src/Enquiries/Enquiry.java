d3f3b5c4d3e16b51f5750d900c7818d3f5a0b1be
Evan Ang <81945199+EvanAngQiXuan@users.noreply.github.com>
2025-04-14 00:41:20 +0800

    Updated Enquiry and Reply

--- src/Enquiries/Enquiry.java
+++ src/Enquiries/Enquiry.java
@@ -1,9 +1,15 @@
 package Enquiries;
 
 import Users.Applicant;
+
+import java.util.List;
+
+import Misc.AccessControl;
 import Projects.*;
+import Users.*;
 
 public class Enquiry {
+    private static int enquiryCounter = 0; // Static counter to generate unique IDs for enquiries
     private Integer id;
     private Applicant applicant;
     private String enquiryString;
@@ -17,13 +23,46 @@ public class Enquiry {
         this.enquiryString = enquiryString;
         this.reply = null; // Initially, there is no reply
         this.project = project; // Set the project related to this enquiry
+        enquiryCounter = id + 1; // Increment the counter for the next enquiry
+    }
+
+    public Integer getEnquiryCounter() {
+        return enquiryCounter;
     }
 
     public Integer getId() {
         return id;
     }
 
+    public Applicant getApplicant() {
+        return applicant;
+    }
+
+    public String getEnquiryString() {
+        return enquiryString;
+    }
+
+    public Reply getReply() {
+        return reply;
+    }
+
     public Project getProject() {
         return project;
     }
+
+    public void setReply(Reply reply) {
+        this.reply = reply;
+    }
+
+    public List<Enquiry> viewEnquiries(User user) {
+        List<Enquiry> all_enquiries = project.getEnquiryList();
+        
+        AccessControl<Enquiry> accessControl = new AccessControl<>();
+        List<Enquiry> readibleEnquiries = all_enquiries.stream()
+                .filter(enquiry -> accessControl.checkAccess(enquiry, user).contains("R"))
+                .toList();
+
+        return readibleEnquiries;
+    }
+
 }
--- src/Enquiries/Reply.java
+++ src/Enquiries/Reply.java
@@ -1,5 +1,47 @@
 package Enquiries;
 
-public class Reply {
+import java.sql.Date;
 
+public class Reply {
+    private Enquiry enquiry;
+    private Date dateTime;
+    private String replyString;
+
+    public Reply(Enquiry enquiry, Date dateTime) {
+        this.enquiry = enquiry;
+        this.dateTime = dateTime;
+        this.replyString = null; // Initially, there is no reply string
+    }
+
+    public Reply(Enquiry enquiry, Date dateTime, String replyString) {
+        this.enquiry = enquiry;
+        this.dateTime = dateTime;
+        this.replyString = replyString;
+    }
+
+    public Enquiry getEnquiry() {
+        return enquiry;
+    }
+
+    public Date getDateTime() {
+        return dateTime;
+    }
+
+    public String getReplyString() {
+        return replyString;
+    }
+
+    public void writeReply(String replyString) {
+        this.replyString = replyString;
+        this.enquiry.setReply(this); // Set the reply in the enquiry
+    }
+
+    public void editReply(String replyString) {
+        this.replyString = replyString;
+    }
+
+    public void deleteReply() {
+        enquiry.setReply(null);
+        this.enquiry = null; // Clear the reference to the enquiry
+    }
 }
