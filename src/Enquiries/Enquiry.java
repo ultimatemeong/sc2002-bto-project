0acd74bcd7a1533cc0bb38a5c2f4af90cea6aa67
Evan Ang <81945199+EvanAngQiXuan@users.noreply.github.com>
2025-04-23 13:46:53 +0800

    Javadoc

--- src/Enquiries/Enquiry.java
+++ src/Enquiries/Enquiry.java
@@ -7,7 +7,14 @@ import Users.Applicant;
 import Users.User;
 import java.time.LocalDateTime;
 import java.util.List;
-
+/**
+ * The Enquiry class represents an enquiry made by an applicant regarding a project.
+ * An apllicant can make multiple enquiries, buy each enquiry will have 0 or 1 reply.
+ * Each enquiry is related to a specific project
+ * @author Ang Qi Xuan Evan
+ * @version 1.0
+ * @since 2025-04-24
+ */
 public class Enquiry {
     private static int enquiryCounter = 1; // Static counter to generate unique IDs for enquiries
     private Integer id;
@@ -18,6 +25,14 @@ public class Enquiry {
     
     private Project project; // The project related to this enquiry
 
+    /**
+     * Constructor to create an Enquiry object.
+     * @param id The unique ID of the enquiry.
+     * @param applicant The applicant who made the enquiry.
+     * @param enquiryString The content of the enquiry.
+     * @param dateTime The date and time when the enquiry was made.
+     * @param project The project related to this enquiry.
+     */
     public Enquiry(Integer id, Applicant applicant, String enquiryString, LocalDateTime dateTime, Project project) {
         this.id = id;
         this.applicant = applicant;
@@ -29,42 +44,82 @@ public class Enquiry {
         
     }
 
+    /**
+     * Gets the unique ID for the next Enquiry.
+     * @return the Counter for number of Enquiries.
+     */
     public static Integer getEnquiryCounter() {
         return enquiryCounter;
     }
 
+    /**
+     * Sets the unique ID for the thiis Enquiry.
+     * @return the ID of this enquiry.
+     */
     public Integer getId() {
         return id;
     }
 
+    /**
+     * Gets the applicant who made the enquiry.
+     * @return the applicant who made the enquiry.
+     */
     public Applicant getApplicant() {
         return applicant;
     }
 
+    /**
+     * Gets the content of the enquiry.
+     * @return the content of the enquiry.
+     */
     public String getEnquiryString() {
         return enquiryString;
     }
 
+    /**
+     * Gets the reply to the enquiry.
+     * @return the reply to the enquiry.
+     */
     public Reply getReply() {
         return reply;
     }
 
+    /**
+     * Gets the date and time when the enquiry was made.
+     * @return the date and time of the enquiry.
+     */
     public LocalDateTime getDateTime() {
         return dateTime;
     }
 
+    /**
+     * Gets the project related to the enquiry.
+     * @return the project related to the enquiry.
+     */
     public Project getProject() {
         return project;
     }
 
+    /**
+     * Sets the project related to the enquiry.
+     * @param project The project related to the enquiry.
+     */
     public void setEnquiryString(String enquiryString) {
         this.enquiryString = enquiryString;
     }
 
+    /**
+     * Sets the reply to the enquiry.
+     * @param reply The reply to the enquiry.
+     */
     public void setReply(Reply reply) {
         this.reply = reply;
     }
 
+    /**
+     * Convers the enquiry to a string representation.
+     * * @return the string representation of the enquiry.
+     */
     public String toString(){
         StringBuilder sb = new StringBuilder();
         sb.append(id).append(",").append(applicant.getNric()).append(",").append(enquiryString).append(",");
@@ -72,6 +127,12 @@ public class Enquiry {
         return sb.toString();
     }
 
+    /** 
+     * This method filters the list of enquiries based on the access control rules.
+     * @param enquiryList The list of enquiries to filter.
+     * @param user The user whose permissions are being checked.
+     * @return A list of enquiries that the user has permission to view.
+     */
     public static List<Enquiry> viewEnquiries(List<Enquiry> enquiryList, User user) {
         AccessControl<Enquiry> accessControl = new EnquiryAccess();
         List<Enquiry> readibleEnquiries = enquiryList.stream()
@@ -81,6 +142,12 @@ public class Enquiry {
         return readibleEnquiries;
     }
 
+    /**
+     * This method searches for an enquiry by its ID in a list of enquiries.
+     * @param enquiryList
+     * @param id
+     * @return The enquiry object with the specified ID, or null if not found.
+     */
     public static Enquiry getEnquiryById(List<Enquiry> enquiryList, Integer id) {
         for (Enquiry enquiry : enquiryList) {
             if (enquiry.getId().equals(id)) {
