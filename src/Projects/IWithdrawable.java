8feb21f58820afaa830df1e2e46ab85565bc09db
Evan Ang <81945199+EvanAngQiXuan@users.noreply.github.com>
2025-04-24 16:26:01 +0800

    Create IWithdrawable

---
+++ src/Projects/IWithdrawable.java
@@ -0,0 +1,10 @@
+package Projects;
+
+/**
+ * IWithdrawable is an interface that defines methods for managing the withdrawal status of applications.
+ * It includes methods to get and set the withdrawal status of an application.
+ */
+public interface IWithdrawable {
+    public String getWithdrawalStatus();
+    public void setWithdrawalStatus(String withdrawalStatus);
+}
